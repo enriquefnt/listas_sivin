a72ec4fb35c0d828210368e4343e1251ccd5a97f
enriquefnt <enriquefinetti@gmail.com>
2022-05-26 07:26:18 -0300

    detalles

--- templates/home.html.php
+++ templates/home.html.php
@@ -9,8 +9,7 @@
 </div>
 <div class="w3-responsive">
 <ul>
-	
-	
+
 
 Opciones del menú:<br><br>
 <li><b>Inicio:</b> Esta pantalla.<br></li>
@@ -21,8 +20,9 @@ if ($_SESSION['tipo']== "SI"){ ?>
 <li><b><a href="../include/ultimos.php" class="w3-bar-block">Ultimos movimientos:</a></b>  Listado nominal de los últimos movimentos en el sistema<br></li>
 <?php }
 ?>
+
 <li><b><a href="../include/parte_internado.php" class="w3-bar-block">Parte internación: </a></b> Lista en el día seleccionado los casos registrados en internación, con la fecha de ingreso, motivo, fecha de egreso (si esta de alta) y el estado (ingreso, internado, alta).<br></li>
-<li><b><a href="../include/paraMCDA.php" class="w3-bar-block">Casos para MCDA: </a></b> Lista de casos mayoeres de 6 meses con indicador antropométrico de Desnutrición Aguda (IMC/Edad < -2 Z.<br></li>
+<li><b><a href="../include/paraMCDA.php" class="w3-bar-block">Casos para MCDA: </a></b> Lista de casos mayores de 6 meses con indicador antropométrico de Desnutrición Aguda (IMC/Edad < -2 Z.<br></li>
 <li><b><a href="../include/act_vigilancia.php" class="w3-bar-block">Actividad de vigilancia: </a></b> Lista las notificaciones, controles y altas registradas por cada vigilante por mes en el período indicado.<br></li>
 <li><b><a href="../include/logout.php"class="w3-bar-block ">Salir: </a></li></b> Sale del sistema y desconecta al usuario. Para volver a ingresar debe indicar su nombre de usuario y contraseña.<br></li>
 
