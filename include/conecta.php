f9ce32fb4f95b8c4bff007bd3ce044ec5d490b80
Enrique Finetti <enriquefinetti@gmail.com>
2022-09-05 07:04:25 -0300

    Update conecta.php

--- include/conecta.php
+++ include/conecta.php
@@ -2,7 +2,7 @@
 
 try {
 $connect = new PDO('mysql:host=200.45.111.88;dbname=MSP_NUTRICION;
-charset=utf8', 'SiViNSalta', '@#sivin#@salta!%2022&&');
+charset=utf8', 'SiViNSalta', '@#sivin#@salta!%2022%%');
 $connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 }
